c5b51771b4599e3aca4401d5749d5d67eb26ea86
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2026-08-11 19:13:14 +0200

    Hello World

---
+++ Source/Program.cs
@@ -0,0 +1,29 @@
+﻿using Verse;
+
+namespace CaravanAdventures
+{
+    public class Main : Mod
+    {
+        //internal static ModSettings Settings { get; set; }
+
+        public Main(ModContentPack content) : base(content)
+        {
+            Log.Message("Hello World! It had loaded YOHO");
+            //Settings = GetSettings<ModSettings>();
+            //HarmonyPatcher.RunEarlyPatches();
+        }
+
+        /*
+        public override void DoSettingsWindowContents(UnityEngine.Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+            GetSettings<ModSettings>().DoWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Caravan Adventures";
+        }
+        */
+    }
+}
